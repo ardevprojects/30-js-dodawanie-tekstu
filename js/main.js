31921ae32303f12b676aeec5e126d617effa1282
arudenko <rudandrzej@gmail.com>
2022-10-25 11:50:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let myParagraph = document.querySelector("p");
+// myParagraph.textContent = "Text";
+myParagraph.textContent = "My Paragraph Text Content";
+console.log(myParagraph);
+
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'My Heading';
+console.log(myHeading);
